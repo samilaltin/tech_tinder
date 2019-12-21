774c5bf7c9df23d3880089a8b8cae7655d647900
burhankarakurt <burhan.karakurt@loodos.com>
2019-12-21 20:50:47 +0300

    dev get list worked

--- lib/screens/MainPage.dart
+++ lib/screens/MainPage.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:tech_tinder/models/Content.dart';
 import 'package:tech_tinder/utility/Constants.dart';
 import 'package:tech_tinder/widgets/SwipeCards.dart';
 
@@ -13,25 +14,25 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   TinderSwapCard cards;
-  List<String> welcomeImages = [
-    "assets/welcome0.png",
-    "assets/welcome1.png",
-    "assets/welcome2.png",
-    "assets/welcome2.png",
-    "assets/welcome1.png",
-    "assets/welcome1.png"
-  ];
-
+List<Content> _list=List();
   @override
   void initState() {
     super.initState();
     FirebaseDatabase firebaseDatabase = FirebaseDatabase();
     DatabaseReference ref = firebaseDatabase.reference();
 
-    ref.child("contents").onValue.listen((e) {
+    ref
+        .child("contents")
+        .onValue
+        .listen((e) {
       DataSnapshot datasnapshot = e.snapshot;
       setState(() {
-        log(datasnapshot.value.toString());
+        Map<dynamic, dynamic> fridgesDs = datasnapshot.value['value'];
+        fridgesDs.forEach((key, value) {
+          if (value) {
+            _list.add(value);
+          }
+        });
       });
     });
   }
@@ -39,19 +40,32 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     CardController controller; //Use this to trigger swap.
-    DatabaseReference ref;
 
     cards = new TinderSwapCard(
         orientation: AmassOrientation.TOP,
         totalNum: 6,
         stackNum: 3,
         swipeEdge: 4.0,
-        maxWidth: MediaQuery.of(context).size.width * 0.9,
-        maxHeight: MediaQuery.of(context).size.width * 0.9,
-        minWidth: MediaQuery.of(context).size.width * 0.8,
-        minHeight: MediaQuery.of(context).size.width * 0.8,
-        cardBuilder: (context, index) => Card(
-              child: Image.asset('${welcomeImages[index]}'),
+        maxWidth: MediaQuery
+            .of(context)
+            .size
+            .width * 0.9,
+        maxHeight: MediaQuery
+            .of(context)
+            .size
+            .width * 0.9,
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width * 0.8,
+        minHeight: MediaQuery
+            .of(context)
+            .size
+            .width * 0.8,
+        cardBuilder: (context, index) =>
+            Card(
+              child: Image.network(
+                  "https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687"),
             ),
         cardController: controller = CardController(),
         swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
@@ -108,8 +122,8 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
     }
   }
 
-  MaterialButton _buildCircularBtn(
-      double height, String img, int type, Function press) {
+  MaterialButton _buildCircularBtn(double height, String img, int type,
+      Function press) {
     double imageSize;
 
     if (type == 1 || type == 4) {
