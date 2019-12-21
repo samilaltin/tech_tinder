d6d68c2481988f8dd19520be5c73f627ccea012a
Şamil <altinsamil@gmail.com>
2019-12-21 14:18:03 +0300

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tech_tinder/LoginPage.dart';
 import 'package:flutter/services.dart';
-import 'package:tech_tinder/screens/Splash.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -15,12 +14,11 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Tech Tinder',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: new Color(0xff075E54),
-        accentColor: new Color(0xff25D366),
+        primarySwatch: Colors.blue,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
