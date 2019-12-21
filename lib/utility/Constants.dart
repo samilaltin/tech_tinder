3e3e544b9d9d5ef7fc8f5cd400225f4a6ecfdf4e
Mert Tutsak <mert.tutsak@loodos.com>
2019-12-21 15:28:37 +0300

    Shared was added

---
+++ lib/utility/Constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  // Shared Preferences keys
+  static final String SP_TOKEN_KEY = "SP_TOKEN";
+}
---
+++ lib/utility/SharedPreferencesHelper.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'Constants.dart';
+
+class SharedPreferencesHelper {
+  static Future<SharedPreferences> getSharedPreferences() async {
+    return await SharedPreferences.getInstance();
+  }
+
+  static void clearSharedPreference() {
+    getSharedPreferences().then((pref) {
+      pref.clear();
+    });
+  }
+
+  static putString(String value, String key) {
+    getSharedPreferences().then((pref) {
+      pref.setString(key, value);
+    });
+  }
+
+  static putBoolean(bool value, String key) {
+    getSharedPreferences().then((pref) {
+      pref.setBool(key, value);
+    });
+  }
+
+  static putJson(Map body, String key) async {
+    await getSharedPreferences().then((pref) {
+      pref.setString(key, json.encode(body));
+    });
+  }
+
+  static Future<String> getString(String key) async {
+    String text = await getSharedPreferences().then((pref) async {
+      return await pref.get(key);
+    });
+    return (text == null ? "" : text);
+  }
+
+  static Future<bool> getBoolean(String key) async {
+    bool val = await getSharedPreferences().then((pref) async {
+      return await pref.get(key);
+    });
+    return val;
+  }
+}
