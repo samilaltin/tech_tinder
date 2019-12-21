85ad1c17f37bbe9b934f5cb0f25f99411173e3dd
Salih <salih.diken@loodos.com>
2019-12-21 13:02:47 +0300

    debug mode banner false

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tech Tinder',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: new Color(0xff075E54),
         accentColor: new Color(0xff25D366),
