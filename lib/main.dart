bd3771b23869c6997c5f9e3a6588a982ca7fbe4c
Salih <salih.diken@loodos.com>
2019-12-21 13:08:29 +0300

    portrait mode enable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tech_tinder/screens/Splash.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       title: 'Tech Tinder',
       debugShowCheckedModeBanner: false,
